7f00d31206b5407335603242c66550d589bed8a9
GipiHippi <gianphilipp@gmail.com>
2016-10-11 08:31:10 +0200

    Update script05.py

--- pi/callscripts/save/script05.py
+++ pi/callscripts/save/script05.py
@@ -6,6 +6,7 @@ import datetime #Anrufszeit auslesen
 import plivo, plivoxml #um die plivo Api zu nutzen
 import json #damit phyton den jason text filtern kann
 import logging #logfile
+import RPi.GPIO as GPIO
 
 #diese Script ist noch leer solage es keine 5en Alarm button im werk 1, 2 oder 3 gibt
 
